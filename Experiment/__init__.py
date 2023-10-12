280c8f6079b18035d0c22ed8edef80ae8d7c8889
alexstevens3 <120400781+alexstevens3@users.noreply.github.com>
2023-10-12 15:06:57 +0200

    Add files via upload

--- Experiment/__init__.py
+++ Experiment/__init__.py
@@ -1,4 +1,6 @@
 from otree.api import *
+#import numpy as np
+import random
 
 
 doc = """
@@ -12,11 +14,30 @@ class C(BaseConstants):
     NUM_ROUNDS = 10
     MAXIMUM_EM = cu(10)
     currency_range(0, 10, 1)
+    payment_MOP1 = 1 
+    payment_MOP2 = 1
+    payment_MOP3 = 1
+    #prob_MOP2 = random.randint(0,100)
 
 
 class Subsession(BaseSubsession):
     pass
 
+def creating_session(subsession: Subsession):
+    import random
+
+    for player in subsession.get_players():
+        player.completion_code = random.randint(0,100)
+
+    #def decision_seller(subsession: Subsession):
+    #    prob_MOP2 = random.randint(0,100)
+    #        if prob_MOP2 >= 81
+    #            return 'MOP2_accepted'
+    #        else:
+    #            return 'MOP2_not_accepted'
+
+    #def decision_seller(subsession):
+    #    prob_MOP2 = random.randint(0,100)       
 
 class Group(BaseGroup):
     pass
@@ -45,6 +66,8 @@ class Player(BasePlayer):
     PaymentChoice_Check = models.BooleanField(
         label = ""
     )
+    completion_code = models.IntegerField()
+    
 
 # PAGES
 class Treatment(Page):
@@ -73,8 +96,6 @@ class PaymentChoice(Page):
             return 'The numbers must add up to 10'
 
 
-    #Validating multiple fields together, see documentation forms; MOP 1 + MOP 2 + MOP 3 = N, if not: error message
-
 class PaymentChoice_Check(Page):
     form_model = 'player'
     form_fields = ['PaymentChoice_Check']
@@ -82,9 +103,64 @@ class PaymentChoice_Check(Page):
 class ResultsWaitPage(WaitPage):
     pass
 
+class Trading_MOP2accept(Page):
+    #@staticmethod
+    #def vars_for_template(player):
+    #    prob_MOP2 = random.randint(0,100)
+    #    return dict(
+    #        prob = prob_MOP2
+    #)
 
-class Results(Page):
-    pass
+    @staticmethod
+    def is_displayed(player):
+        #prob_MOP2 = random.randint(0,100)
+        return player.completion_code <= 81
+
+    #@staticmethod
+    #def is_displayed(player):
+        #prob_MOP2 = random.randint(0,100)
+    #    return subsession.decision_seller <= 81
+
+class Trading_MOP2notaccept(Page):
+    #@staticmethod
+    #def vars_for_template(player):
+    #    prob_MOP2 = random.randint(0,100)
+    #    return dict(
+    #        prob = prob_MOP2
+    #)
+
+    @staticmethod
+    def is_displayed(player):
+        #prob_MOP2 = random.randint(0,100)
+        return player.completion_code > 81
+    
+    #def prob_MOP2(player):
+     #   prob_MOP2 = random.randint(0,100)
+    #prob = random.randint(0,100)
+
+    #@staticmethod
+    #def vars_for_template(player: Player):
+    #    prob = random.randint(0,100)
+        #if prob <= 81:
+        #    return dict(
+        #        MOP = MOP 1, MOP 2, MOP 3)
+        #else:
+        #    return dict(
+        #        MOP = MOP 1, MOP 3)
+        
+    
+    #@staticmethod
+    #def decision_seller(player: Player):
+    #    prob_MOP2 = random.randint(0,100)
+    #    if prob <= 81:
+    #        return 'All 3 MOP accepted' 
+    #    else:
+    #        return 'MOP 1,3'
+
+    #accepted_MOP = random.randint()
+    #prob_accept_MOP2 = 0.81
+    #prob_accept_MOP1 = 1 
 
 
-page_sequence = [Treatment, CBDCChoice, PaymentChoice, PaymentChoice_Check, ResultsWaitPage, Results]
+
+page_sequence = [Treatment, CBDCChoice, PaymentChoice, PaymentChoice_Check, ResultsWaitPage, Trading_MOP2accept, Trading_MOP2notaccept]
