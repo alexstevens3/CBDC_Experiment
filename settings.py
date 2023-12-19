f15489355ea51d919cf926c2213be25fc6db8909
alexstevens3 <120400781+alexstevens3@users.noreply.github.com>
2023-12-19 13:46:13 +0100

    Update settings.py
    change app sequence

--- settings.py
+++ settings.py
@@ -3,7 +3,7 @@ from os import environ
 SESSION_CONFIGS = [
      dict(
          name='CBDC_Experiment',
-         app_sequence=['survey','testquestions','Testround','Experiment'],
+         app_sequence=['testquestions','Testround','Experiment', 'survey'],
          num_demo_participants=2,
      ),
 ]
